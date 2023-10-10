d76858e51d6c854317352cb940648f072fbfde9b
reddragonex6 <lsantos7654@gmail.com>
2023-10-10 16:05:36 -0400

    Update test.cpp

--- RPI_led/test/test.cpp
+++ RPI_led/test/test.cpp
@@ -4,26 +4,43 @@
 
 #define GPIO_CHIP "gpiochip0"
 #define GPIO_PIN_17 17
+#define GPIO_PIN_27 27
 
 int main()
 {
     gpiod::chip chip(GPIO_CHIP);
-    gpiod::line line = chip.get_line(GPIO_PIN_17);
+    gpiod::line line1 = chip.get_line(GPIO_PIN_17);
+    gpiod::line line2 = chip.get_line(GPIO_PIN_27);
 
     // Check if the line is available for use
-    if (!line.is_requested())
+    if (!line1.is_requested())
     {
-        line.request({ "gpio_toggle_led", gpiod::line_request::DIRECTION_OUTPUT });
+        line1.request({ "gpio_toggle_led", gpiod::line_request::DIRECTION_OUTPUT });
+    }
+    if (!line2.is_requested())
+    {
+        line2.request({ "gpio_toggle_led", gpiod::line_request::DIRECTION_OUTPUT });
     }
 
-    int value = 0;
+    int value1 = 0;
+    int value2 = 0;
+    int choice = 0;
 
     while(1)
     {
-        value = !value; // Toggle value
-        line.set_value(value);
+        std::cout << "1: LED 17, 0: LED_27" << std::endl;
+        choice = std::cin.get();
+        
+        if (choice){
+            value1 = !value1; // Toggle value
+            line1.set_value(value1);
+            std::cout << "LED 17 set to " << (value1 ? "ON" : "OFF") << std::endl;
+        }else{
+            value2 = !value2; // Toggle value
+            line2.set_value(value2);
+            std::cout << "LED 27 set to " << (value2 ? "ON" : "OFF") << std::endl;
+        }
 
-        std::cout << "LED set to: " << (value ? "ON" : "OFF") << std::endl;
 
         std::cin.get();
     }
