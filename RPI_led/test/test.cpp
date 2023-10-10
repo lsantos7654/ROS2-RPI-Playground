8abf2a40caabd32947660425b546dd515ff18441
reddragonex6 <lsantos7654@gmail.com>
2023-10-10 15:48:50 -0400

    Update test.cpp

--- RPI_led/test/test.cpp
+++ RPI_led/test/test.cpp
@@ -1,27 +1,32 @@
-// File: gpio_listener.cpp
 #include <iostream>
 #include <gpiod.hpp>
-
+#include <unistd.h>
 
 #define GPIO_CHIP "gpiochip0"
-#define GPIO_PIN_1 17
-#define GPIO_PIN_2 27
-
-gpiod::chip chip(GPIO_CHIP);
-gpiod::line line1 = chip.get_line(GPIO_PIN_1);
-gpiod::line line2 = chip.get_line(GPIO_PIN_2);
-
+#define GPIO_PIN_17 17
 
 int main()
 {
-    std::string check;
+    gpiod::chip chip(GPIO_CHIP);
+    gpiod::line line = chip.get_line(GPIO_PIN_17);
+
+    // Check if the line is available for use
+    if (!line.is_requested())
+    {
+        line.request({ "gpio_toggle_led", gpiod::line_request::DIRECTION_OUTPUT });
+    }
+
     int value = 0;
+
     while(1)
     {
-        value = !value;
-        line1.set_value(value);
-        std::cout << value << std::endl;
-        
-        std::cin.get();
+        value = !value; // Toggle value
+        line.set_value(value);
+
+        std::cout << "LED set to: " << (value ? "ON" : "OFF") << std::endl;
+
+        sleep(1); // Sleep for 1 second before toggling again
     }
+
+    return 0;
 }
