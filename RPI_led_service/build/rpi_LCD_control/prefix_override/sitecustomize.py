92fb2e98d1dcf7a3e3f1051733b28067a497cbc5
lsantos7654 <lsantos7654@gmail.com>
2023-10-24 19:55:03 -0400

    cleanup

--- RPI_led_service/build/rpi_LCD_control/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/pi/Sandbox/RPI_led_service/install/rpi_LCD_control'
--- RPI_led_service/build/rpi_gpio/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/pi/Sandbox/RPI_led_service/install/rpi_gpio_msgs/share/rpi_gpio_msgs/package.sh"
